ca2c2796c91eb7bff13e0a43fbbfd807c7245420
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2024-10-05 18:12:41 -0600

    test cambiados

--- test.py
+++ test.py
@@ -1,36 +1,43 @@
 import unittest
-from url_validator_dfa import URLValidatorDFA 
+from AFD import Automata
+from validator_txt import ValidatorTxt
+from leer_afd_txt import leer_automata_de_txt
 
 class TestURLValidator(unittest.TestCase):
 
     def setUp(self):
-        self.validator = URLValidatorDFA()
+        self.data = leer_automata_de_txt('automata.txt')
+        self.validatorData = ValidatorTxt(self.data) 
+        if self.validatorData.validar_existencia_alfabeto():
+            self.validator = Automata(self.data)
+        else:
+            raise Exception('Define bien el automata')
 
     # Clase válida
     def test_valid_url_with_protocol(self):
-        self.assertTrue(self.validator.is_valid_url('https://www.google.com'))
+        self.assertTrue(self.validator.validar_cadena('https://www.google.com'))
     
     def test_valid_url_without_protocol(self):
-        self.assertTrue(self.validator.is_valid_url('http://www.example.com'))
+        self.assertTrue(self.validator.validar_cadena('http://www.example.com'))
     
     def test_valid_url_with_port_and_params(self):
-        self.assertTrue(self.validator.is_valid_url('https://example.com/path'))
+        self.assertTrue(self.validator.validar_cadena('https://example.com/path'))
 
     # Clase inválida
     def test_invalid_protocol(self):
-        self.assertFalse(self.validator.is_valid_url('htp://wrong'))
+        self.assertFalse(self.validator.validar_cadena('htp://wrong'))
     
     def test_invalid_non_url_string(self):
-        self.assertFalse(self.validator.is_valid_url('notAURL'))
+        self.assertFalse(self.validator.validar_cadena('notAURL'))
     
     def test_invalid_url_with_special_characters(self):
-        self.assertFalse(self.validator.is_valid_url('https://example?.com'))
+        self.assertFalse(self.validator.validar_cadena('https://example?.com'))
 
     def test_empty_string(self):
-        self.assertFalse(self.validator.is_valid_url(''))
+        self.assertFalse(self.validator.validar_cadena(''))
 
     def test_url_with_spaces(self):
-        self.assertFalse(self.validator.is_valid_url('https ://example.com'))
+        self.assertFalse(self.validator.validar_cadena('https ://example.com'))
 
 if __name__ == '__main__':
     unittest.main()
